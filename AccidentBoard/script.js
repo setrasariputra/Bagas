ef10f6c0e3c92df053fb44bd77e59ae5907696a8
Wisnu K. Ardianto <setrasariputra@gmail.com>
2024-11-07 21:02:57 +0700

    Create javascript module SafetyPerformanceDate

---
+++ AccidentBoard/script.js
@@ -0,0 +1,26 @@
+class SafetyPerformanceBoard {
+    constructor() {
+        this.run();
+    }
+
+    run() {
+        this.liveDateTime();
+    }
+
+    liveDateTime() {
+        setInterval(this.updateDateTime, 1000);
+    }
+
+    updateDateTime() {
+        const now = new Date();
+        document.getElementById("currentDate").innerHTML = now.toLocaleDateString();
+        document.getElementById("currentTime").innerHTML = now.toLocaleTimeString();
+    }
+
+    addAccidentRecord() {
+        
+    }
+}
+
+
+new SafetyPerformanceBoard();
