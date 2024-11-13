e80bd9f63a066b0c34f21c5a833f9f5570074174
Wisnu K. Ardianto <setrasariputra@gmail.com>
2024-11-13 22:07:44 +0700

    add final validation input form

--- AccidentBoard/script.js
+++ AccidentBoard/script.js
@@ -26,8 +26,17 @@ class SafetyPerformanceBoard {
             const accidentDate = document.getElementById("accidentDate").value;
             const accidentCount = parseInt(document.getElementById("accidentCount").value);
             
+            // add double validation value is not empty
             if(accidentDate && accidentCount > 0) {
-                console.log("proses simpan data")
+                // next validation futureDate
+                const today = new Date().setHours(0, 0, 0, 0);
+                const selectedDate = new Date(accidentDate).setHours(0, 0, 0, 0);
+                
+                if(selectedDate > today) {
+                    alert("Tidak dapat menambahkan data kecelakaan untuk tanggal dimasa depan.")
+                }else{
+                    console.log("simpan data");
+                }
             }
         });
     }
