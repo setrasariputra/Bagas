b36c1cbcea3ea3717d9e467554bf5ef7064f628c
Wisnu K. Ardianto <setrasariputra@gmail.com>
2024-12-22 16:01:09 +0700

    Create encryption base64 encode

--- AccidentBoard/login.js
+++ AccidentBoard/login.js
@@ -1,5 +1,18 @@
-document.getElementById("login-form").addEventListener("submit", (e) => {
+document.getElementById("login-form").addEventListener("submit",(e)=>{
     e.preventDefault();
 
-    alert("tes");
-});
+    const userlogin = document.getElementById("userlogin").value;
+    const userpass = document.getElementById("userpass").value;
+    const userPassString=userpass.toString();
+
+    // encrypt credential
+    const rawCredential = userlogin+userPassString;
+    const base64Encryption = btoa(rawCredential); // model base64 encryption
+    
+    if(base64Encryption === 'c2FmZXR5MDAwMDAw') {
+        alert("login berhasil")
+    }
+    else{
+        alert("login gagal")
+    }
+})
