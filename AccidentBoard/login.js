240641364c646aa36c25377c72dde0876c13fe7b
Wisnu K. Ardianto <setrasariputra@gmail.com>
2024-12-22 19:11:02 +0700

    modify login.js

--- AccidentBoard/login.js
+++ AccidentBoard/login.js
@@ -10,14 +10,21 @@ document.getElementById("login-form").addEventListener("submit",(e)=>{
     const base64Encryption = btoa(rawCredential); // model base64 encryption
     
     if(base64Encryption === 'c2FmZXR5MDAwMDAw') {
-        alert("login berhasil")
+        // save to localStorage
+        // set auth
+        const auth = 1;
+        localStorage.setItem("auth", auth);   
+        
+        // set session expired
+        const now = new Date();
+        const timestamp = Date.now();
+        const expiredDuration = 60 * 1000 // 60 second
+        const expiredTimestamp = timestamp + expiredDuration;
+        localStorage.setItem('session', expiredTimestamp);
+
         // redirect ke dashboard
         const dashbordUrl = 'http://127.0.0.1:5500/AccidentBoard/dashboard.html';
         window.location.href = dashbordUrl;
-
-        // save to localStorage
-        const auth = 1;
-        localStorage.setItem("auth", auth);
     }
     else{
         alert("login gagal")
