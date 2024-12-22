1177fdfbd0869319c9e050aa9073681b9af6c83c
Wisnu K. Ardianto <setrasariputra@gmail.com>
2024-12-22 19:24:02 +0700

    create class auth

--- AccidentBoard/auth.js
+++ AccidentBoard/auth.js
@@ -1,3 +1,43 @@
+class auth {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        //this.testAlert();
+        this.logout();
+    }
+
+    testAlert() {
+        alert('hello tes from class');
+    }
+
+    authValidation() {
+
+    }
+
+    sessionValidation() {
+
+    }
+
+    logout() {
+        document.getElementById("auth-logout").addEventListener("click", (e) => {
+            e.preventDefault();
+
+            alert('click logout');
+        });
+    }
+
+    redirectToLogin() {
+        const loginUrl = 'http://127.0.0.1:5500/AccidentBoard/login.html';
+        window.location.href = loginUrl;
+    }
+}
+
+new auth();
+
+
+/*
 // validasi auth
 const auth = localStorage.getItem("auth");
 const authNumber = Number(auth);
@@ -12,3 +52,4 @@ if(authNumber === 1) {
     const loginUrl = 'http://127.0.0.1:5500/AccidentBoard/login.html';
     window.location.href = loginUrl;
 }
+*/
