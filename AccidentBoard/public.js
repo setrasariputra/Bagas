eb16cc2f9ad353f214f20849458088c0150f7499
Wisnu K. Ardianto <setrasariputra@gmail.com>
2025-01-06 22:27:10 +0700

    create display date circle

--- AccidentBoard/public.js
+++ AccidentBoard/public.js
@@ -6,6 +6,7 @@ class publicBoard {
     run(){
         this.getCurrentYear();
         this.getCurrentMonth();
+        this.displayDateCalendar();
     }    
 
     getCurrentYear() {
@@ -17,7 +18,22 @@ class publicBoard {
         const currentMonthIndex = new Date().getMonth();
         const monthInIndonesian = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "Nopember", "Desember"];
         document.getElementById("currentMonth").innerHTML=monthInIndonesian[currentMonthIndex];
-    }    
+    }
+    
+    getCurrentTotalDayInMonth() {
+
+    }
+
+    displayDateCalendar() {
+        const totalDateInMonth = 31;
+        let dateCircle = "";
+        for(let i = 0; i < totalDateInMonth; i++) {
+            const date = i + 1;
+            dateCircle += "<div class='day-value'>"+date+"</div>";    
+        }
+
+        document.getElementById("displayDateCalendar").innerHTML=dateCircle;
+    }
 }
 
 new publicBoard();
